6f32ee081b0fa733329d358e2377e91cc760f84a
Hikari_Coder <khanhkhoi08@gmail.com>
2026-09-09 16:00:21 +0700

    Initial commit

---
+++ src/main/java/com/laundry/backend/LaundryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laundry.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryBackendApplication.class, args);
+	}
+
+}
